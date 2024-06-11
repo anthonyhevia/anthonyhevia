88fa0a37ebf04fdfc0659bc6a405321ed1f9dbae
Anthony Hevia <anthonyxhevia@gmail.com>
2024-06-11 14:41:02 -0500

    Fixed formatting

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
-$(function(){ 
-    $("#header").load("../../header.html");  
-  }); 
+$(function () {
+	$("#header").load("../../header.html");
+});
 
-$(function(){
-    $("#footer").load("../../footer.html");
+$(function () {
+	$("#footer").load("../../footer.html");
 })
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,25 +5,25 @@ const liElements = ulElement.querySelectorAll("li");
 let manualNavigation = false;
 
 liElements.forEach(li => {
-  li.addEventListener("click", () => {
-    liElements.forEach(item => item.querySelector("a").classList.remove("active"));
-    li.querySelector("a").classList.add("active");
-    manualNavigation = true;
+	li.addEventListener("click", () => {
+		liElements.forEach(item => item.querySelector("a").classList.remove("active"));
+		li.querySelector("a").classList.add("active");
+		manualNavigation = true;
 
-    setTimeout(() => {
-      manualNavigation = false;
-    }, 3000); 
-  });
+		setTimeout(() => {
+			manualNavigation = false;
+		}, 3000);
+	});
 });
 
 const sections = ['#about', '#projects', '#research', '#contact'];
 let sectionOffsets = {};
 
 function updateOffsets() {
-  sectionOffsets = {};
-  sections.forEach(section => {
-    sectionOffsets[section] = document.querySelector(section).offsetTop;
-  });
+	sectionOffsets = {};
+	sections.forEach(section => {
+		sectionOffsets[section] = document.querySelector(section).offsetTop;
+	});
 }
 
 updateOffsets();
@@ -33,37 +33,37 @@ window.addEventListener('resize', updateOffsets);
 let lastSection = null;
 
 function activateCurrentSection() {
-  if (manualNavigation) return;
+	if (manualNavigation) return;
 
-  let scrollPositionTop = window.scrollY;
-  let scrollPositionMiddle = scrollPositionTop + window.innerHeight / 2;
-  let scrollPositionBottom = scrollPositionTop + window.innerHeight;
+	let scrollPositionTop = window.scrollY;
+	let scrollPositionMiddle = scrollPositionTop + window.innerHeight / 2;
+	let scrollPositionBottom = scrollPositionTop + window.innerHeight;
 
-  let currentSection = null;
+	let currentSection = null;
 
-  if (scrollPositionTop <= sectionOffsets['#about']) {
-    currentSection = '#about';
-  } else if (scrollPositionBottom >= document.body.scrollHeight) {
-    currentSection = '#contact';
-  } else {
-    for (let section of sections) {
-      if (scrollPositionMiddle >= sectionOffsets[section]) {
-        currentSection = section;
-      } else {
-        break;
-      }
-    }
-  }
+	if (scrollPositionTop <= sectionOffsets['#about']) {
+		currentSection = '#about';
+	} else if (scrollPositionBottom >= document.body.scrollHeight) {
+		currentSection = '#contact';
+	} else {
+		for (let section of sections) {
+			if (scrollPositionMiddle >= sectionOffsets[section]) {
+				currentSection = section;
+			} else {
+				break;
+			}
+		}
+	}
 
-  if (currentSection !== lastSection) {
-    lastSection = currentSection;
-    
-    liElements.forEach(item => item.querySelector("a").classList.remove("active"));
-    const activeLink = ulElement.querySelector(`a[href='/#${currentSection.substring(1)}']`);
-    if (activeLink) {
-      activeLink.classList.add("active");
-    }
-  }
+	if (currentSection !== lastSection) {
+		lastSection = currentSection;
+
+		liElements.forEach(item => item.querySelector("a").classList.remove("active"));
+		const activeLink = ulElement.querySelector(`a[href='/#${currentSection.substring(1)}']`);
+		if (activeLink) {
+			activeLink.classList.add("active");
+		}
+	}
 }
 window.addEventListener('load', activateCurrentSection)
 window.addEventListener('scroll', activateCurrentSection);
